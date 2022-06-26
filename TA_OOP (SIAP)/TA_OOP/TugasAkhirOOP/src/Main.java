b915aa7bbcef55439671df4e63caaa32b0362b0f
YOHANES RICKY KURNIAWAN <101657987+yohanesricky17@users.noreply.github.com>
2022-06-26 20:01:30 +0700

    Update Main.java

--- TA_OOP (SIAP)/TA_OOP/TugasAkhirOOP/src/Main.java
+++ TA_OOP (SIAP)/TA_OOP/TugasAkhirOOP/src/Main.java
@@ -1,3 +1,8 @@
+// Aldi Dwi Purnomo – 2440065346
+// Jayawardhana Bayu Krisnatama EkaPutra – 2440100632
+// Yohanes Ricky Kurniawan – 2440056480
+// Ernes Gideon Geonanda Senong – 2440080012
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
